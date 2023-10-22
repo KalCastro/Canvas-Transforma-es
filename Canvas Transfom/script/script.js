0f972fe77e8c260dff0fdd060c94100bb0e34d3c
KalCastro <127865406+KalCastro@users.noreply.github.com>
2023-10-22 18:16:52 -0300

    Update script.js

--- Canvas Transfom/script/script.js
+++ Canvas Transfom/script/script.js
@@ -6,8 +6,8 @@ let offsetX, offsetY;
 
 const img = new Image();
 img.src = 'imgs/emoji.png';
-let imgX = 0;
-let imgY = 0;
+let imgX = canvas.width / 2; // Inicializa a imagem no centro horizontal
+let imgY = canvas.height / 2; // Inicializa a imagem no centro vertical
 let imgScale = 0.3;
 let imgRotation = 0;
 
@@ -19,35 +19,26 @@ function drawImage() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   ctx.save();
-  
-  ctx.translate(canvas.width / 2, canvas.height / 2);
-  
+
+  ctx.translate(imgX, imgY); // Use imgX e imgY como coordenadas de translação
   ctx.rotate(imgRotation);
-  
+
   ctx.scale(imgScale, imgScale);
-  
+
   // Defina um caminho de recorte em torno da imagem
   ctx.beginPath();
   ctx.arc(0, 0, img.width / 2, 0, Math.PI * 2, false);
   ctx.clip();
 
   ctx.drawImage(img, -img.width / 2, -img.height / 2);
-  
+
   ctx.restore();
 }
 
 // Função para manipular eventos de teclado
 function handleKeyPress(event) {
   const step = 5;
-  if (event.key === 'ArrowUp') {
-    imgY -= step;
-  } else if (event.key === 'ArrowDown') {
-    imgY += step;
-  } else if (event.key === 'ArrowLeft') {
-    imgX -= step;
-  } else if (event.key === 'ArrowRight') {
-    imgX += step;
-  } else if (event.key === 'a') {
+  if (event.key === 'a') {
     imgRotation += Math.PI / 180; // Rotação de 1 grau
   } else if (event.key === 'd') {
     imgRotation -= Math.PI / 180; // Rotação no sentido oposto de 1 grau
@@ -55,9 +46,17 @@ function handleKeyPress(event) {
     imgScale *= 1.1; // Aumenta a escala em 10%
   } else if (event.key === 's') {
     imgScale /= 1.1; // Diminui a escala em 10%
-  } else if (event.key === 'q') { // Adicionar esta condição para lidar com a tecla 'q'
+  } else if (event.key === 'q') {
     imgRotation = 0; // Redefine a rotação para 0 graus
     imgScale = 0.3; // Redefine a escala para 100%
+  } else if (event.key === 'ArrowLeft') {
+    imgX -= step; // Move a imagem para a esquerda
+  } else if (event.key === 'ArrowRight') {
+    imgX += step; // Move a imagem para a direita
+  } else if (event.key === 'ArrowUp') {
+    imgY -= step; // Move a imagem para cima
+  } else if (event.key === 'ArrowDown') {
+    imgY += step; // Move a imagem para baixo
   }
   drawImage();
 }
